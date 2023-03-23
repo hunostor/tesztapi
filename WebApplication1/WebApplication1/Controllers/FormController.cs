fd1d78ee1b3f8634fb1e71e8ecfe9e9a6324b3cf
Attila Poroszkai <poroszkai.attila@gmail.com>
2023-03-23 19:52:42 +0100

    form controller

---
+++ WebApplication1/WebApplication1/Controllers/FormController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FormController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetGenders")]
+        public IEnumerable<string> GetGenders()
+        {
+            var responseList = new List<string>()
+            {
+                "man",
+                "woman"
+            };
+
+            return responseList;
+
+        }
+
+        [HttpPost]
+        [Route("Input")]
+        public FormInput Input([FromBody] FormInput input)
+        {
+
+            return input;
+
+        }
+
+        [HttpGet]
+        [Route("GetExclusionConditions")]
+        public IEnumerable<string> GetExclusionConditions()
+        {
+            var responseList = new List<string>()
+            {
+                "glutént tartalmaz",
+                "tejet tartalmaz",
+                "mogyoró tartalmú",
+                "húst tartalmaz",
+                "állati eredetüt tartalmaz",
+                "diót tartalmaz",
+                "epret tartalmaz",
+                "szója tartalmaz",
+                "mustár tartalmaz",
+                "zeller tartalmaz",
+                "szezámmag tartalmaz",
+            };
+
+            return responseList;
+            
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/FormInput.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class FormInput
+    {
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public int Heigth { get; set; }
+        public int Weight { get; set; }
+    }
+}
